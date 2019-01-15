ccd385ef52df888c47749f38c57c73ca1b404a1e
Noor <aftab22n@mtholyoke.edu>
2019-01-15 11:20:19 -0500

    Merge branches 'Alexis's_Branch' and 'master' of https://github.com/snehakanaujia/DemoGitUse
    # Conflicts:
    #	app/src/main/java/com/example/demouser/demogituse/MainActivity.java

--- app/src/main/java/com/example/demouser/demogituse/MainActivity.java
+++ app/src/main/java/com/example/demouser/demogituse/MainActivity.java
@@ -86,15 +86,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_info) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_checklist) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_journal) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_resources) {
 
         } else if (id == R.id.nav_send) {
 
