144575f95228f9c1a95c6cba083f63d2d4959fd6
Alexis <ferro22a@mtholyoke.edu>
2019-01-15 11:40:57 -0500

    Changed home view text from "Noor did this!" to "Welcome to our app to better the health of the MHC community!" in the content_main xml file.

--- app/src/main/java/com/example/demouser/demogituse/MainActivity.java
+++ app/src/main/java/com/example/demouser/demogituse/MainActivity.java
@@ -78,21 +78,26 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home)
+        {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        }
+        else if (id == R.id.nav_info)
+        {
 
-        } else if (id == R.id.nav_slideshow) {
+        }
+        else if (id == R.id.nav_checklist) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_journal) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_gratitudeAndAffirmations) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_resources) {
 
         }
 
