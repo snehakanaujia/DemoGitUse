c0763210eda6d4f29f428efafff98eba1e095977
Alexis <ferro22a@mtholyoke.edu>
2019-01-16 11:37:55 -0500

    Added functionality for check boxes to interact with progress bar when the check box is clicked.

--- app/src/main/java/com/example/demouser/demogituse/MainActivity.java
+++ app/src/main/java/com/example/demouser/demogituse/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.demouser.demogituse;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
@@ -13,9 +16,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ProgressBar pB;
+    CheckBox physical, spiritual, mental, emotional;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +32,13 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+        emotional = (CheckBox)findViewById(R.id.EmotionalCheckBox);
+        spiritual = (CheckBox)findViewById(R.id.SpiritualCheckBox);
+        mental = (CheckBox)findViewById(R.id.MentalCheckBox);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -106,5 +121,64 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void physicalCheckBx()
+    {
+        pB = (ProgressBar)findViewById(R.id.progressBar);
+        physical = (CheckBox)findViewById(R.id.PhysicalCheckBox);
+
+        physical.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                pB.setProgress(10);
+            }
+        });
+    }
+
+    public void emotionalCheckBx()
+    {
+        pB = (ProgressBar) findViewById(R.id.progressBar);
+        emotional = (CheckBox)findViewById(R.id.EmotionalCheckBox);
+
+        emotional.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                pB.setProgress(10);
+            }
+        });
+    }
+
+    public void spiritualCheckBx()
+    {
+        pB = (ProgressBar) findViewById(R.id.progressBar);
+        spiritual = (CheckBox)findViewById(R.id.SpiritualCheckBox);
+
+        emotional.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                pB.setProgress(10);
+            }
+        });
+
+    }
+
+    public void mentalCheckBx()
+    {
+        pB = (ProgressBar) findViewById(R.id.progressBar);
+        mental = (CheckBox)findViewById(R.id.MentalCheckBox);
+
+        mental.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.N)
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                pB.setProgress(10, true);
+            }
+        });
     }
 }
